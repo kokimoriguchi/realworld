3fd99d5978c2223dfa2f5370dfa4b34f160211b0
kokimoriguchi <skoki5231@icloud.com>
2023-06-16 22:28:38 +0900

    article作成と表示完成

--- frontend/src/components/pages/Header.jsx
+++ frontend/src/components/pages/Header.jsx
@@ -1,9 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../hooks/Auth";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { auth } = useContext(AuthContext);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setIsLoggedIn(!!auth);
+  }, [auth]);
   const handleHomeClick = () => {
-    navigate("/");
+    navigate("home");
   };
 
   const handleSignInClick = () => {
@@ -13,31 +21,63 @@ const Header = () => {
   const handleSignUpClick = () => {
     navigate("create_user");
   };
+
+  const handleArticleClick = () => {
+    navigate("createArticle");
+  };
+
+  const handleLoginHomeClick = () => {
+    navigate("loginHome");
+  };
   return (
-    <div className="flex justify-between px-40 h-40">
-      <div className="pt-5">
+    <div className="flex justify-between items-center px-40 h-14">
+      <div className="">
         <p className="text-3xl text-green-500">conduit</p>
       </div>
-      <div className="pt-7">
-        <ul className="md:flex space-x-4">
-          <li
-            className="text-gray-400 hover:text-gray-500"
-            onClick={handleHomeClick}
-          >
-            Home
-          </li>
-          <li
-            className="text-gray-400 hover:text-gray-500"
-            onClick={handleSignInClick}
-          >
-            Sign in
-          </li>
-          <li
-            className="text-gray-400 hover:text-gray-500"
-            onClick={handleSignUpClick}
-          >
-            Sign up
-          </li>
+      <div className="">
+        <ul className="md:flex space-x-3 ">
+          {isLoggedIn ? (
+            <>
+              <li
+                className="text-gray-400 hover:text-gray-500"
+                onClick={handleLoginHomeClick}
+              >
+                Home
+              </li>
+
+              <li
+                className="text-gray-400 hover:text-gray-500"
+                onClick={handleArticleClick}
+              >
+                🗒️New Article
+              </li>
+              <li className="text-gray-400 hover:text-gray-500">
+                {auth.user.name}
+              </li>
+            </>
+          ) : (
+            <>
+              <li
+                className="text-gray-400 hover:text-gray-500"
+                onClick={handleHomeClick}
+              >
+                Home
+              </li>
+
+              <li
+                className="text-gray-400 hover:text-gray-500"
+                onClick={handleSignInClick}
+              >
+                Sign in
+              </li>
+              <li
+                className="text-gray-400 hover:text-gray-500"
+                onClick={handleSignUpClick}
+              >
+                Sign up
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
